07bc278e49569e4608fbd8241793f71a98528cbc
strider <prerakmody@gmail.com>
2019-05-25 17:35:49 +0200

    Added a method to automatically generate .txt files for VOC

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,8 +3,10 @@ import cv2
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-
 from PIL import Image
+
+import pickle
+import xml.etree.ElementTree as ET
 
 import torch
 import torch.utils.data as data
@@ -210,6 +212,58 @@ def load_data_detection(imgpath, shape, jitter, hue, saturation, exposure):
     label = fill_truth_detection(labpath, img.width, img.height, flip, dx, dy, 1./sx, 1./sy)
     return img,label
 
+
+## --------------------------------------- PASCAL VOC - v2 (voc_label.py) --------------------------------------- ##
+def convert(size, box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = (box[0] + box[1])/2.0
+    y = (box[2] + box[3])/2.0
+    w = box[1] - box[0]
+    h = box[3] - box[2]
+    x = x*dw
+    w = w*dw
+    y = y*dh
+    h = h*dh
+    return (x,y,w,h)
+
+def convert_annotation(DATA_DIR, year, image_id):
+    classes  = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+    in_file  = open(os.path.join(DATA_DIR, 'VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id)))
+    out_file = open(os.path.join(DATA_DIR,'VOCdevkit/VOC%s/labels/%s.txt'%(year, image_id)), 'w')
+    tree     = ET.parse(in_file)
+    root     = tree.getroot()
+    size     = root.find('size')
+    w        = int(size.find('width').text)
+    h        = int(size.find('height').text)
+
+    for obj in root.iter('object'):
+        difficult = obj.find('difficult').text
+        cls       = obj.find('name').text
+        if cls not in classes or int(difficult) == 1:
+            continue
+        cls_id = classes.index(cls)
+        xmlbox = obj.find('bndbox')
+        b      = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text), float(xmlbox.find('ymax').text))
+        bb     = convert((w,h), b)
+        out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+
+def setup_VOC(DATA_DIR):
+
+    sets    = [('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+
+    for year, image_set in sets:
+        print (' - year : ', year, ' || image_set : ', image_set)
+        if not os.path.exists(os.path.join(DATA_DIR, 'VOCdevkit/VOC%s/labels/'%(year))):
+            os.makedirs(os.path.join(DATA_DIR, 'VOCdevkit/VOC%s/labels/'%(year)))
+
+        image_ids = open(os.path.join(DATA_DIR, 'VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set))).read().strip().split()
+        list_file = open(os.path.join(DATA_DIR, 'VOCdevkit/%s_%s.txt'%(year, image_set)), 'w')
+        for image_id in image_ids:
+            list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg\n'%(DATA_DIR, year, image_id))
+            convert_annotation(DATA_DIR, year, image_id)
+        list_file.close()
 
 ## --------------------------------------- PASCAL VOC - v1 --------------------------------------- ##
 
